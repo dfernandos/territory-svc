d5411878c70b525187770a5fe9b5fb4c9ae8c5b8
Daniel Oliveira <danieloliveira@MacBook-Pro-de-Daniel.local>
2023-07-26 13:07:09 -0300

    database config

--- src/main/java/com/mapeando/territory/config/MongoConfig.java
+++ src/main/java/com/mapeando/territory/config/MongoConfig.java
@@ -1,24 +1,24 @@
-//package com.mapeando.territory.config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.mongodb.core.MongoTemplate;
-//import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
-//
-//@Configuration
-//public class MongoConfig {
-//
-//    private final DatabaseConfigProperties properties;
-//
-//    @Autowired
-//    public MongoConfig(DatabaseConfigProperties properties) {
-//        this.properties = properties;
-//    }
-//
-//    @Bean
-//    public MongoTemplate mongoTemplate() {
-//        return new MongoTemplate(new SimpleMongoClientDatabaseFactory(properties.getUri()));
-//    }
-//}
-//
+package com.mapeando.territory.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoConfig {
+
+    private final DatabaseConfigProperties properties;
+
+    @Autowired
+    public MongoConfig(DatabaseConfigProperties properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(new SimpleMongoClientDatabaseFactory(properties.getUri()));
+    }
+}
+
